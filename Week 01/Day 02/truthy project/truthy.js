a63b6b5d75605932dd9b8a035882a752ed1e4e75
matt-codeslo <matt@codeslo.com>
2018-12-06 17:51:18 -0800

    added comments to truthy.js

--- Week 01/Day 02/truthy project/truthy.js
+++ Week 01/Day 02/truthy project/truthy.js
@@ -1,3 +1,5 @@
+// lessThanFive shold return the number passed to it if it is less than five. Number larger than that return false. Passing something that is not a number should return 0
+
 function lessThanFive(num){
     if(typeof num !== 'number'){
         return 0;
@@ -8,6 +10,8 @@ function lessThanFive(num){
         return false;
     }
 }
+
+// arrayToString returns a string made up of all the elements of an array. If passed something that is not an array, it should return false
 
 function arrayToString(arr){
     let result = "";
